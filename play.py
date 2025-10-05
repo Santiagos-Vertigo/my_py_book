5dc0f1311983e4d6303c0b06055ea1665bc7319c
Santiagos-Vertigo <dgo.narvaez@gmail.com>
2025-10-05 16:57:07 -0500

    lists

---
+++ play.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'bird']
+pizzas = ['peperoni', 'margarita', 'cheese']
+
+for animal in animals:
+    print(f"\nA {animal} would make a great pet")
+print("Any o these animals would make a great pet!")
